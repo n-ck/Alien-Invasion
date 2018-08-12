a71f1e55c120a5422bffd18c98d36eb4525ec97e
Nick Kuizenga <nickkuis@hotmail.com>
2018-08-12 12:17:59 -0400

    Ship shooots bullets, left off on page 261: Deleting Old Bullets

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
 
 import pygame
+from pygame.sprite import Group
 
 from settings import Settings
 from ship import Ship
@@ -16,6 +17,8 @@ def run_game():
 
 	# Make a ship.
 	ship = Ship(ai_settings, screen)
+	# Make a group to store bullets in.
+	bullets = Group()
 
 	# Set the background color.
 	bg_color = (ai_settings.bg_color)
@@ -23,9 +26,10 @@ def run_game():
 	# Start the main loop for the game.
 	while True:
 
-		gf.check_events(ship)
+		gf.check_events(ai_settings, screen, ship, bullets)
 		ship.update()
-		gf.update_screen(ai_settings, screen, ship)
+		bullets.update()
+		gf.update_screen(ai_settings, screen, ship, bullets)
 
 
 run_game()
