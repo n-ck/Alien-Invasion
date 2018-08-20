60d133640cffd48449abccd610e4396fd24aa38f
Nick Kuizenga <nickkuis@hotmail.com>
2018-08-19 20:15:08 -0400

    Increased speed with every level, left off on page 301 - Scoring

--- game_functions.py
+++ game_functions.py
@@ -48,6 +48,9 @@ def check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bul
 	"""Starts a new game when the player clicks Play."""
 	button_clicked = play_button.rect.collidepoint(mouse_x, mouse_y)
 	if button_clicked and not stats.game_active:
+		# Reset the game settings.
+		ai_settings.initialize_dynamic_settings()
+
 		# Hide the mouse cursor.
 		pygame.mouse.set_visible(False)
 
@@ -98,8 +101,9 @@ def check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets):
 	collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
 
 	if len(aliens) == 0:
-		# Destroy existing bullets and create new fleet.
+		# Destroy existing bullets, speed up game, and create new fleet.
 		bullets.empty()
+		ai_settings.increase_speed()
 		create_fleet(ai_settings, screen, ship, aliens)
 
 def get_number_aliens_x(ai_settings, alien_width):
